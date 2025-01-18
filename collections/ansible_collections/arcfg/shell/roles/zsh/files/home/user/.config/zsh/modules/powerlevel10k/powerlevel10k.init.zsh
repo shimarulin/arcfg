03c845d7ae1539444adc80bf6d9e3e8b8e8ba865
Vyacheslav Shimarulin <shimarulin@gmail.com>
2025-01-18 22:21:50 +0300

    refactor(shell): powerlevel10k prompt

--- collections/ansible_collections/arcfg/shell/roles/zsh/files/home/user/.config/zsh/modules/powerlevel10k/powerlevel10k.init.zsh
+++
@@ -1,8 +0,0 @@
-# To customize prompt, run `p10k configure` or edit ~/.config/zsh/.p10k.zsh.
-
-# Detect Linux console can be possible with "$TERM" = "linux" or `tput colors` = "8"
-if [ `tput colors` = "8" ]; then
-  [[ ! -f ~/.config/zsh/modules/powerlevel10k/themes/p10k.tty.zsh ]] || source ~/.config/zsh/modules/powerlevel10k/themes/p10k.tty.zsh
-else
-  [[ ! -f ~/.config/zsh/modules/powerlevel10k/themes/p10k.zsh ]] || source ~/.config/zsh/modules/powerlevel10k/themes/p10k.zsh
-fi
